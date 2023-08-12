2954455a45874d83ee3cc2518f5d3608d309f91e
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2023-08-12 15:00:07 -0300

    :children_crossing: add route get peoples

--- modelo-3/api/index.js
+++ modelo-3/api/index.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
-
-app.use(bodyParser.json());
-
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send({ messagem: 'Hello World!' });
-});
+routes(app);
 
 app.listen(port, () => {
   console.log(`Server running http://localhost:${port}`);
